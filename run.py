ab9fb3d6be8ea34c1e28f726629056398580c7a3
WojtekKamilowski <kamil.wojciechowski105@gmail.com>
2023-01-09 06:44:45 +0000

    Add update_worksheet(data, worksheet) function

--- run.py
+++ run.py
@@ -55,5 +55,15 @@ def validate_data(values):
     
     return True
 
-data = get_loaded_data()
-print(data)
+
+def update_worksheet(data, worksheet):
+    """
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully.\n")
+
+
